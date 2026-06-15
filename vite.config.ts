f1ffbf005dca33bdfe1a05f1fb8786eaf8bf944e
Austin Riha <austin.w.riha@gmail.com>
2026-06-15 01:19:34 -0500

    Change base path in vite.config.ts to root

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
 export default defineConfig(({mode, command}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: command === 'build' ? '/Tutoring_Website/' : '/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
